c5f3a645052a22610cdfbc61db5b5989d887b546
Manixa-Brat <admin@manix.tk>
2017-03-09 19:52:00 +0200

    capitalize and fix require

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ use Manix\Brat\Program;
 define('PUBLIC_PATH', __DIR__);
 define('PROJECT_PATH', realpath(__DIR__ . '/../project'));
 
-require PROJECT_PATH . '/../vendor/manix/brat/src/manix.php';
+require __DIR__ . '/../vendor/manix/brat/src/manix.php';
 
 $manix->run(new class extends Program {
     
