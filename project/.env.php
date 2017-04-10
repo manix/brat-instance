bcb9379e3f1145edfb30bc93e176925c7f08f62c
manix <admin@manix.info>
2017-04-10 08:32:01 +0300

    env

--- project/.env.php
+++ project/.env.php
@@ -2,4 +2,18 @@
 
 return [
     'env' => 'debug',
+    'db' => [
+        'host' => '',
+        'dbname' => '',
+        'user' => '',
+        'pass' => '',
+        'charset' => 'utf8'
+    ],
+    'mail' => [
+        'host' => '',
+        'port' => 465,
+        'encryption' => 'ssl',
+        'user' => '',
+        'pass' => '',
+    ],
 ];
