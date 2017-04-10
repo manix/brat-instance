baf5fa90df146a8b265135b25526bb3170d9a66e
manix <admin@manix.info>
2017-04-10 08:40:42 +0300

    user management*

--- project/Controllers/Index.php
+++ project/Controllers/Index.php
@@ -10,7 +10,7 @@ class Index extends Controller {
   public $page = IndexView::class;
 
   public function get() {
-    \Manix\Brat\Utility\Users\Models\Auth::required();
+    
     return [
         'heading' => $this->t8('common', 'indexh'),
         'paragraph' => $this->t8('common', 'indexp')
