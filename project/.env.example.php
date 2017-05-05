17e6664a7fc21465529e05bbb6699d303d3a1b7f
manix <admin@manix.info>
2017-05-05 13:28:56 +0300

    add example env

---
+++ project/.env.example.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'env' => 'debug',
+    'db' => [
+        'host' => '',
+        'dbname' => '',
+        'user' => '',
+        'pass' => '',
+        'charset' => 'utf8'
+    ],
+    'mail' => [
+        'host' => '',
+        'port' => 465,
+        'encryption' => 'ssl',
+        'user' => '',
+        'pass' => '',
+    ],
+];
