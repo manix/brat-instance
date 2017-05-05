b2682f83e87fc4637fdff6fd14872492e38cb26e
manix <admin@manix.info>
2017-05-05 13:15:49 +0300

    up with plugins

--- project/config/routes.php
+++ project/config/routes.php
@@ -1,7 +1,5 @@
-<?php
-
-return [
-    '' => 'Project\\Controllers\\',
-    'users' => 'Manix\\Brat\\Utility\\Users\\Controllers\\',
-    'captcha' => Manix\Brat\Utility\Captcha\CaptchaController::class
-];
+<?php return array (
+  '' => 'Project\\Controllers\\',
+  'users' => 'Manix\\Brat\\Utility\\Users\\Controllers\\',
+  'captcha' => 'Manix\\Brat\\Utility\\Captcha\\CaptchaController',
+);
