e16f798ed5413ece31aa5374f3c8bf6f05c6a3a9
manix <admin@manix.info>
2017-05-06 21:51:35 +0300

    new plugin installer method

--- project/config/scripts.php
+++ project/config/scripts.php
@@ -2,8 +2,10 @@
 
 use Manix\Brat\Utility\Scripts\Help;
 use Manix\Brat\Utility\Scripts\RunSQL;
+use Manix\Brat\Utility\Scripts\RunPlugin;
 
 return [
     'sql' => RunSQL::class,
-    'help' => Help::class
+    'help' => Help::class,
+    'plugin' => RunPlugin::class
 ];
