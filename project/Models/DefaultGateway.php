7d6c53a3242f6d1b99a1cf8e93e7bc3256b41c6b
manix <admin@manix.info>
2017-07-31 13:32:21 +0300

    fix opening a new connection every time

--- project/Models/DefaultGateway.php
+++ project/Models/DefaultGateway.php
@@ -7,20 +7,34 @@ use PDO;
 
 class DefaultGateway extends SQLGateway {
 
-  function __construct() {
-    $host = $_ENV['db']['host'] ?? null;
-    $dbname = $_ENV['db']['dbname'] ?? null;
-    $charset = $_ENV['db']['charset'] ?? null;
-    $user = $_ENV['db']['user'] ?? null;
-    $pass = $_ENV['db']['pass'] ?? null;
-    
-    $pdo = new PDO("mysql:host={$host};dbname={$dbname};charset={$charset};", $user, $pass, [
-        PDO::ATTR_EMULATE_PREPARES => false,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ]);
+  protected static $connections = [];
 
-    parent::__construct($pdo);
+  public static connect($name) {
+    if (!isset(static::$connections[$name])) {
+      $host = $_ENV[$name]['host'] ?? null;
+      $dbname = $_ENV[$name]['dbname'] ?? null;
+      $charset = $_ENV[$name]['charset'] ?? null;
+      $user = $_ENV[$name]['user'] ?? null;
+      $pass = $_ENV[$name]['pass'] ?? null;
+
+      $pdo = new PDO("mysql:host={$host};dbname={$dbname};charset={$charset};", $user, $pass, [
+          PDO::ATTR_EMULATE_PREPARES => false,
+          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+          PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+      ]);
+
+      static::$connections[$name] = $pdo;
+    }
+
+    return static::$connections[$name];
+  }
+
+  public function getPDO() {
+    return $this->pdo;
+  }
+
+  function __construct($connection = 'db') {
+    parent::__construct($connection instanceof PDO ? $connection : static::connect($connection));
   }
 
 }
