0c3e059b5f096108784581ea91f4732c01311835
Manix <admin@manix.info>
2018-08-10 12:52:23 +0300

    make auth request callable on middleware

--- project/Middleware/Auth.php
+++ project/Middleware/Auth.php
@@ -6,16 +6,19 @@ use Manix\Brat\Components\Controller;
 use Manix\Brat\Components\Errors\Exception;
 use Manix\Brat\Components\Middleware;
 use Manix\Brat\Components\Program;
-use Manix\Brat\Utility\Users\Controllers\Login;
 use Manix\Brat\Utility\Users\Models\Auth as AuthFacade;
+use Project\Controllers\Users\Login;
 
 class Auth implements Middleware {
 
-  public function execute(Controller $controller, $method, Program $program) {
-
+  public static function request() {
     if (!AuthFacade::user()) {
       throw (new Exception("Log in", 403))->setHandler(new Login());
     }
+  }
+
+  public function execute(Controller $controller, $method, Program $program) {
+    static::request();
   }
 
 }
